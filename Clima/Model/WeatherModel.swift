c63286540fb88c0585b183013ba7f4de31ab1f68
ElchinNasirov <nasirov.elchin@gmail.com>
2022-03-06 19:43:00 -0800

    Created WeatherModel struct to move over some functionality and build the MVC pattern.

---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Elchin Nasirov on 3/6/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    
+}
